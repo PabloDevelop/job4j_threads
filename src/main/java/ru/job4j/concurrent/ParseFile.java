2ee31f411afe48c718b7a0b690fe61856c807e58
pavelberzkaln <p.berzkaln@gmail.com>
2022-12-15 23:31:08 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+    private final File file;
+
+    public ParseFile(final File file) {
+        this.file = file;
+    }
+
+    public synchronized String genContent(Predicate<Character> filter) {
+        String output = "";
+        try (BufferedInputStream i = new BufferedInputStream(new FileInputStream(file))) {
+            int data;
+            while ((data = i.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output += (char) data;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+}
---
+++ src/main/java/ru/job4j/concurrent/WriteFile.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public final class WriteFile {
+    private final File file;
+
+    public WriteFile(final File file) {
+        this.file = file;
+    }
+
+    public synchronized void saveContent(String content) {
+        try (BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
