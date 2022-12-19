fb83c2ec088fc5aecbfe04796b7be904dea0b571
pavelberzkaln <p.berzkaln@gmail.com>
2022-12-19 22:53:49 +0300

    3. Денежные переводы AccountStorage [#1104 #372170]

--- src/main/java/ru/job4j/concurrent/AccountStorage.java
+++ src/main/java/ru/job4j/concurrent/AccountStorage.java
@@ -8,7 +8,7 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public synchronized boolean add(Account account) {
-        return accounts.put(account.id(), account) != null;
+        return accounts.put(account.id(), account) == null;
     }
 
     public synchronized boolean update(Account account) {
