edca4ed5fa22f066acde90c55cf9c5587c10df98
pavelberzkaln <p.berzkaln@gmail.com>
2022-12-21 22:17:35 +0300

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/concurrent/CountBarrier.java
@@ -0,0 +1,48 @@
+package ru.job4j.concurrent;
+
+/**
+ * Разработайте класс, который блокирует выполнение по условию счетчика.
+ */
+public class CountBarrier {
+    private final Object monitor = this;
+
+    /**
+     * Переменная total содержит количество вызовов метода count().
+     */
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    /**
+     * Метод count изменяет состояние программы.
+     * Это значит, что внутри метода count нужно вызывать метод notifyAll.
+     */
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+        }
+    }
+
+    /**
+     * Нити, которые выполняют метод await,
+     * могут начать работу если поле count >= total.
+     * Если оно не равно, то нужно перевести нить в состояние wait.
+     * Здесь нужно использовать цикл while для проверки состояния, а не оператор if.
+     */
+    public void await() {
+        synchronized (monitor) {
+            while (count < total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
