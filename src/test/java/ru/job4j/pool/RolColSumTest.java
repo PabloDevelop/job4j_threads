28f59b5eebb55cbb2fe91f7cb64a8f5e875f4b05
pavelberzkaln <p.berzkaln@gmail.com>
2023-01-16 21:32:15 +0300

    4. CompletableFuture [#361626 #381571]

--- src/test/java/ru/job4j/pool/RolColSumTest.java
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RolColSumTest {
     @Test
@@ -14,13 +14,13 @@ class RolColSumTest {
                 {4, 5, 6},
                 {7, 8, 9},
         };
-        RolColSum.Sums[] testArray = {
-                new RolColSum.Sums(6, 12),
-                new RolColSum.Sums(15, 15),
-                new RolColSum.Sums(24, 18),
+        Sums[] testArray = {
+                new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18),
         };
-        RolColSum.Sums[] resultArray = RolColSum.sum(array);
-        assertArrayEquals(testArray, resultArray);
+        Sums[] resultArray = RolColSum.sum(array);
+        assertThat(testArray).isEqualTo(resultArray);
     }
 
     @Test
@@ -30,12 +30,12 @@ class RolColSumTest {
                 {4, 5, 6},
                 {7, 8, 9},
         };
-        RolColSum.Sums[] testArray = {
-                new RolColSum.Sums(6, 12),
-                new RolColSum.Sums(15, 15),
-                new RolColSum.Sums(24, 18),
+        Sums[] testArray = {
+                new Sums(6, 12),
+                new Sums(15, 15),
+                new Sums(24, 18),
         };
-        RolColSum.Sums[] resultArray = RolColSum.asyncSum(array);
-        assertArrayEquals(testArray, resultArray);
+        Sums[] resultArray = RolColSum.asyncSum(array);
+        assertThat(testArray).isEqualTo(resultArray);
     }
 }
